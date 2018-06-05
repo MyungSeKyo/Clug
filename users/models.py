df7541f0e057b00ee62eb3626b8497a7247f8024
oliver <myungsekyo@gmail.com>
2018-06-05 14:38:33 +0900

    유저 모델 폰번호 추가

--- users/models.py
+++ users/models.py
@@ -13,16 +13,15 @@ class User(AbstractBaseUser, PermissionsMixin):
         help_text=_('아이디를 입력해 주세요.'),
         validators=[
             MinLengthValidator(4, '4자 이상 32자 이하로 입력해주세요.'),
-            MaxLengthValidator(32, '4자 이상 32자 이하로 입력해주세요.'),
             RegexValidator(r'^[a-z0-9]+$', '알파벳 소문자와 숫자만 허용됩니다.')
         ],
         error_messages={
             'unique': '이미 존재하는 아이디 입니다.',
+            'max_length': '4자 이상 32자 이하로 입력해주세요.'
         },
     )
     email = models.EmailField(
         '이메일',
-        blank=True,
         unique=True,
         error_messages={
             'unique': '이미 가입된 이메일입니다.'
@@ -32,14 +31,27 @@ class User(AbstractBaseUser, PermissionsMixin):
         '학번',
         max_length=8,
         unique=True,
-        help_text='학번을 입력해 주세요.',
+        help_text='학번을 입력해주세요.',
         validators=[
             MinLengthValidator(8, '8자의 학번을 입력해주세요.'),
-            MaxLengthValidator(8, '8자의 학번을 입력해주세요.'),
             RegexValidator(r'^[0-9]+$', '숫자만 허용됩니다.')
         ],
         error_messages={
-            'unique': '이미 가입된 학번입니다.'
+            'unique': '이미 가입된 학번입니다.',
+            'max_length': '8자의 학번을 입력해주세요.'
+        }
+    )
+    phone_number = models.CharField(
+        '핸드폰 번호',
+        max_length=11,
+        unique=True,
+        help_text='핸드폰 번호를 입력해주세요.',
+        validators=[
+            MinLengthValidator(10),
+            RegexValidator(r'^[0-9]+$')
+        ],
+        error_messages={
+            'unique': '이미 가입된 휴대폰 번호입니다.'
         }
     )
     is_staff = models.BooleanField(
