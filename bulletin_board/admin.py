966fed0e71685a2a05592c0028d1c1987f747e7f
oliver <myungsekyo@gmail.com>
2018-06-05 18:21:28 +0900

    admin에 post model 추가

--- bulletin_board/admin.py
+++ bulletin_board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from bulletin_board.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Post, PostAdmin)
