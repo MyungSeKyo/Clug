49f63a5492364caa15ffa4e63fc62561f1f61570
Unknown <myungsekyo@gmail.com>
2018-06-10 20:11:52 +0900

    Post Form 추가

---
+++ posts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    type = forms.HiddenInput()
+
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'type')
+        widgets = {
+            'content': SummernoteWidget(),
+        }
+
